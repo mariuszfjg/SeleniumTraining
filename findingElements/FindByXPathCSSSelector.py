651e8031e5f15367024d3ce6bb2424598caed00f
mariusz.figiel <mariusz.figiel1@live.com>
2018-02-20 13:47:27 +0100

    Some small rework on refactoring

--- findingElements/FindByXPathCSSSelector.py
+++ findingElements/FindByXPathCSSSelector.py
@@ -6,16 +6,16 @@ class FindByXPathCSSSelector:
 
     def __init__(self):
         self.url = 'https://letskodeit.teachable.com/pages/practice'
-        #self.url = 'https://www.google.com'
+        # self.url = 'https://www.google.com'
 
     def test(self):
         driver = webdriver.Firefox()
         driver.get(self.url)
 
         try:
-            elementByXpath = driver.find_element_by_xpath("//input[@id='name']")
+            element_by_xpath = driver.find_element_by_xpath("//input[@id='name']")
 
-            if elementByXpath is not None:
+            if element_by_xpath is not None:
                 print("Test passes by xpath")
         except sce.NoSuchElementException:
             print("No element found by xpath")
@@ -23,9 +23,9 @@ class FindByXPathCSSSelector:
             pass
 
         try:
-            elementByCss = driver.find_element_by_css_selector("#displayed-text")
+            element_by_css = driver.find_element_by_css_selector("#displayed-text")
 
-            if elementByCss is not None:
+            if element_by_css is not None:
                 print("Test passes by css")
         except sce.NoSuchElementException:
             print("No element found by CSS Selector")
@@ -33,6 +33,7 @@ class FindByXPathCSSSelector:
             pass
 
         driver.quit()
+
 
 if __name__ == "__main__":
     ff = FindByXPathCSSSelector()
