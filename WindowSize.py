5922e2eba66b83c4eb538cce177d8496daf9bc1d
mariuszfjg <figiel.mariusz1@gmail.com>
2018-02-26 16:42:48 +0100

    Getting size of a window using JavaScript method called from WebDriver

---
+++ WindowSize.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+
+class WindowSize:
+
+    def __init__(self):
+        self.url = 'https://www.google.com'
+
+    def test(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(self.url)
+        driver.implicitly_wait(3)
+
+        height = driver.execute_script("return window.innerHeight;")
+        width = driver.execute_script("return window.innerWidth;")
+
+        print("Window has size (H/W) : {0}/{1}".format(str(height),
+                                                       str(width)
+                                                       ))
+
+        driver.quit()
+
+
+def main():
+    windows_size = WindowSize()
+    windows_size.test()
+
+
+if __name__ == '__main__':
+    main()
+
