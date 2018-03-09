1a7f657b4b91828b44f1a07911c772578f8c1f45
mariuszfjg <figiel.mariusz1@gmail.com>
2018-03-09 17:12:30 +0100

    2nd commit

---
+++ JavaScriptPopup.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+class JavaScriptPopup:
+    def __init__(self):
+        pass
+
+    def exercise(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
